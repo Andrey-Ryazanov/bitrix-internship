8a745eb3afab2e3477e8a929c9b185a129d5086d
Andrey-Ryazanov <andrey58645@mail.ru>
2024-04-02 00:36:55 +0300

    Task 3.2: задание выполнено

---
+++ local/include/footer/copyright.php
@@ -0,0 +1,9 @@
+<p>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+    Copyright &copy;</script>
+    <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
+    <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
+    target="_blank">Colorlib</a>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+</p>
+
