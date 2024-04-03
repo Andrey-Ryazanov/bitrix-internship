dab3c8cba8c4f97ddc408bf7d70ef2a11f453ad1
Andrey-Ryazanov <andrey58645@mail.ru>
2024-04-03 23:58:36 +0300

    Task 3.1: обновлено заполнение заголовка, языка и проверка подключения ядра

--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -1,9 +1,9 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?=LANGUAGE_ID?>">
 
 <head>
-    <title>HomeSpace &mdash; Colorlib Website Template</title>
+    <title><?=$APPLICATION->ShowTitle();?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
